9de1a1b85260c1f42c019305e579506e8054be8a
ksongbcit <ksong34@my.bcit.ca>
2022-05-18 18:17:16 -0700

    created models for customer, transaction, address, added gitignore file for project files

---
+++ rdks-webapp/RDKSDatabase/Models/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RDKSDatabase.Models
+{
+    public class Customer
+    {
+
+        [Key]
+        public int CUS_ID { get; set; }
+
+        [StringLength(12)]
+        public string? CUS_ACCNUM { get; set; }
+
+        [StringLength(30)]
+        public string? CUS_COMPNAME { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string? CUS_FNAME { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string? CUS_LNAME { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string? CUS_PHONE { get; set; }
+
+        [StringLength(15)]
+        public string? CUS_ALT_PHONE { get; set; }
+
+        [StringLength(30)]
+        public string? CUS_EMAIL { get; set; }
+
+        [StringLength(30)]
+        public string? CUS_ALT_EMAIL { get; set; }
+
+        [Required]
+        [Column("CUS_ADDRESS")]
+        public int ADDR_ID { get; set; }
+
+        [Column("CUS_ALT_ADDRESS")]
+        public int ALT_ADDR_ID { get; set; }
+
+        [Required]
+        public Boolean CUS_FR { get; set; }
+
+        [Required]
+        public Boolean CUS_TTS { get; set; }
+
+        [Required]
+        public Boolean CUS_MEZ { get; set; }
+
+        [Required]
+        public int CUS_DEACTIVATED_COUNT { get; set; }
+
+        [StringLength(50)]
+        public string? CUS_NOTE { get; set; }
+
+        public ICollection<Address>? Addresses { get; set; }
+
+    }
+}
---
+++ rdks-webapp/RDKSDatabase/Models/Transaction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RDKSDatabase.Models
+{
+    public class Transaction
+    {
+        [Key]
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        public string? TRANS_NUM { get; set; }
+
+        public int CUS_ID { get; set; }
+        public int LICENSE_PLATE { get; set; }
+
+        [DataType()]
+        public DateTime? TRANS_COMPLETION_DATE { get; set; }
+        public string? TRANS_COMPLETION_TIME { get; set; }
+        public string? TRANS_LOAD_NUM { get; set; }
+        public string? TRANS_NETWEIGHT { get; set; }
+        public string? TRANS_TOTALPRICE { get; set; }
+        public string? TRANS_HAULER { get; set; }
+        public string? TRANS_DRIVER_EXEPT_STATUS { get; set; }
+
+        public string? VALID_IMPORT_CODE { get; set; }
+
+        public string? TRANS_CONTRACT { get; set; }
+        public string? TRANS_OPERATION { get; set; }
+        public string? TRANS_STATUS { get; set; }
+        public string? TRANS_ISMANUAL { get; set; }
+        public string? TRANS_HASEXCEPTION { get; set; }
+        public string? TRANS_KEYFIELD { get; set; }
+        public string? TRANS_SOURCE_TYPE { get; set; }
+        public string? TRANS_ADJUSTED_PRICE { get; set; }
+        public string? TRANS_FACILITY_CODE { get; set; }
+        public string? TRANS_TONNES { get; set; }
+        public string? TRANS_CUBIC_METERS { get; set; }
+        public string? TRANS_IN_SERVICE_AREA { get; set; }
+        public string? TRANS_ASC_NON_ASC { get; set; }
+        public string? TRANS_FUNCTION { get; set; }
+        public string? TRANS_CURBSIDE_AREA { get; set; }
+        public string? TRANS_CURBSIDE_STREAM { get; set; }
+        public string? TRANS_ANNUAL_REPORTING_GROUP { get; set; }
+        public string? TRANS_ANNUAL_REPORTING_SOURCE { get; set; }
+        public string? TRANS_SERVICE_AREA { get; set; }
+
+    }
+}
