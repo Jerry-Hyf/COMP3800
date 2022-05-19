6697da1371adf13c4fd6dc8dc292fc95de5a0e39
Jerry Hu <huyifei2018516@gmail.com>
2022-05-19 15:49:20 -0700

    fix conflict between I and Kyung

--- rdks-webapp/RDKSDatabase/Data/RDKSDatabaseContext.cs
+++ rdks-webapp/RDKSDatabase/Data/RDKSDatabaseContext.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
 using RDKS.Models;
-=======
 using RDKSDatabase.Models;
->>>>>>> 868077c4c1d65f7f3fd07ae451970ff19bbdb629
 
 namespace RDKSDatabase.Data
 {
@@ -18,11 +15,9 @@ namespace RDKSDatabase.Data
         {
         }
 
-<<<<<<< HEAD
         public DbSet<RDKS.Models.HWY37N_HAZELTON>? HWY37N_HAZELTON { get; set; }
 
         public DbSet<RDKS.Models.HWY37N_KITWANGA>? HWY37N_KITWANGA { get; set; }
-=======
         public DbSet<RDKSDatabase.Models.Customer>? Customer { get; set; }
         public DbSet<RDKSDatabase.Models.Address>? Addresses { get; set; }
 
@@ -33,6 +28,5 @@ namespace RDKSDatabase.Data
         }
 
         public DbSet<RDKSDatabase.Models.ABCRecycling>? ABCRecycling { get; set; }
->>>>>>> 868077c4c1d65f7f3fd07ae451970ff19bbdb629
     }
 }
