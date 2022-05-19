e9b6dfc69e6e2ee308aa29a9ac6b2b94886624b2
Jerry Hu <huyifei2018516@gmail.com>
2022-05-18 19:49:36 -0700

    remove all float

--- rdks-webapp/RDKSDatabase/Models/HWY37N_HAZELTON.cs
+++ rdks-webapp/RDKSDatabase/Models/HWY37N_HAZELTON.cs
@@ -11,36 +11,28 @@ namespace RDKS.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HWY_HAZ_DATE { get; set; }
-        
-        [Float]
+
         [Display(Name = "HWY_HAZ_EST_OCC_TONNES")]
         public float HWY_HAZ_EST_OCC_TONNES { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_OCC_BIN_BILLING")]
         public float HWY_HAZ_OCC_BIN_BILLING { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_SCRAP_METAL_TONNES")]
         public float HWY_HAZ_SCRAP_METAL_TONNES { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_TIRE_COUNTS")]
         public float HWY_HAZ_TIRE_COUNTS { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_TIRE_COLLECTION_CHARGES")]
         public float HWY_HAZ_TIRE_COLLECTION_CHARGES { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_FREON_REMOVAL_CHARGES")]
         public float HWY_HAZ_FREON_REMOVAL_CHARGES { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_MARR_INCOME")]
         public float HWY_HAZ_MARR_INCOME { get; set; }
 
-        [Float]
         [Display(Name = "HWY_HAZ_ABC_INCOME")]
         public float HWY_HAZ_ABC_INCOME { get; set; }
 
