ffb0facb44b3c24abc88d109d74496242f3a812f
Damon-Jo <lunaticjky@gmail.com>
2022-05-19 12:00:08 -0700

    modify vehicle badge class name and add display name

--- rdks-webapp/RDKSDatabase/Models/Vehicle.cs
+++ rdks-webapp/RDKSDatabase/Models/Vehicle.cs
@@ -8,17 +8,25 @@ namespace RDKSDatabase.Models
     public class Vehicle_badge
     {
         [Key]
+        [Display(Name = "License Plate")]
         [Required]
         [StringLength(10, MinimumLength = 6)]
         public string? LICENSE_PLATE { get; set; }
 
+        [Required]
+        [Display(Name = "Customer account number")]
         public int CUS_ID { get; set; }
+
+        [Display(Name = "Description")]
         public string? DESCRIPTION { get; set; }
 
+        [Display(Name = "Badge")]
         public int BADGE { get; set; }
 
+        [Display(Name = "Notes")]
         public string? NOTES { get; set; }
 
+        [Display(Name = "Notes2")]
         public string? NOTES_2 { get; set; }
 
 
