3e70eb047e82c18e73eca611ab7c16189c48248e
ksongbcit <ksong34@my.bcit.ca>
2022-05-25 01:34:49 -0700

    fixed import_code fk discrepancy for transaction

--- rdks-webapp/RDKSDatabase/Models/Transaction.cs
+++ rdks-webapp/RDKSDatabase/Models/Transaction.cs
@@ -124,7 +124,7 @@ namespace RDKSDatabase.Models
         [Display(Name = "Driver's Exempt Status")]
         public bool TRANS_DRIVER_EXEMPT_STATUS { get; set; }
 
-        [StringLength(10)]
+        [Required]
         [Display(Name = "Material Code")]
         public string? VALID_IMPORT_CODE { get; set; }
 
